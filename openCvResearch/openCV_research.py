e41be764e2d6251526ff999d396ae3074767009e
Cymbre Spoehr <cymbre@mattscott.org>
2022-11-29 20:00:02 -0500

    OpenCV Research

---
+++ openCvResearch/openCV_research.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import imutils
+
+image = cv2.imread("magic-cards.jpg", cv2.IMREAD_COLOR)
+
+image = imutils.resize(image, width = 500)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# blur = cv2.GaussianBlur(gray, (3,3), 0)
+rect, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_TOZERO_INV)
+rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+dilation = cv2.dilate(thresh, rect_kernel, iterations = 1)
+
+contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours = contours[0] if len(contours) == 2 else contours[1]
+threshold_min_area = 400
+number_of_contours = 0
+
+file = open("recognized.txt", "w+")
+file.write("")
+file.close()
+
+for c in contours:
+    area = cv2.contourArea(c)
+    if area > threshold_min_area:
+        cv2.drawContours(image, [c], 0, (36,255,12), 3)
+        number_of_contours += 1
+
+print(number_of_contours)
+cv2.imshow("image", image)
+cv2.imshow("thresh", thresh)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
