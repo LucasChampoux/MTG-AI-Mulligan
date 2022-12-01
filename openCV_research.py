1a38a16659ad696efbed6f7f06bba48e9fadf01f
Cymbre Spoehr <cymbre@mattscott.org>
2022-12-01 17:14:01 -0500

    Reference work for the OCR stuff

--- openCV_research.py
+++ openCV_research.py
@@ -1,8 +1,14 @@
 import cv2
 import numpy as np
 import imutils
+import pytesseract
 
-image = cv2.imread("magic-cards.jpg", cv2.IMREAD_COLOR)
+image = cv2.imread("text.png", cv2.IMREAD_COLOR)
+
+file = open("recognized.txt", "w+")
+custom_config = r'--oem 3 --psm 6'
+file.write(pytesseract.image_to_string(image, config=custom_config))
+file.close()
 
 image = imutils.resize(image, width = 500)
 
@@ -17,9 +23,7 @@ contours = contours[0] if len(contours) == 2 else contours[1]
 threshold_min_area = 400
 number_of_contours = 0
 
-file = open("recognized.txt", "w+")
-file.write("")
-file.close()
+
 
 for c in contours:
     area = cv2.contourArea(c)
@@ -30,6 +34,8 @@ for c in contours:
 print(number_of_contours)
 cv2.imshow("image", image)
 cv2.imshow("thresh", thresh)
+
+
 
 cv2.waitKey(0)
 
