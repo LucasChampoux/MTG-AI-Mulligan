121ace4dea88e11f4cacce94363c05fade34877b
Cymbre Spoehr <cymbre@mattscott.org>
2022-12-02 13:30:11 -0500

    Remove unnecessary prints

--- jsonTester.py
+++ jsonTester.py
@@ -3,11 +3,6 @@ import difflib
 
 jsonFile = open("NamesOnly.json", "r")
 newJson = jsonFile.read().replace(",", "").replace("  ", "").splitlines()
-
-for j in newJson:
-    print(j)
-
-print(len(newJson))
 jsonFile.close()
 
-print(difflib.get_close_matches("[sanctum Weaver 18)", newJson))
+print(difflib.get_close_matches("=", newJson))
