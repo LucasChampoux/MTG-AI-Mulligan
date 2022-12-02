5f6316a93354c04803decee9a69825430039ff9b
Cymbre Spoehr <cymbre@mattscott.org>
2022-12-02 07:57:46 -0500

    Made a lot of changes and added computer vision aspect to try to get the text on each card

--- openCV_research.py
+++ openCV_research.py
@@ -3,32 +3,38 @@ import numpy as np
 import imutils
 import pytesseract
 
-image = cv2.imread("realMagicCards.png", cv2.IMREAD_COLOR)
-
-file = open("recognized.txt", "w+")
-custom_config = r'--oem 3 --psm 6'
-file.write(pytesseract.image_to_string(image, config=custom_config))
-file.close()
-
-image = imutils.resize(image, width = 800)
+image = cv2.imread("realMagicCards.png")
+original_image = image.copy()
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (3,3), 0)
+blur = cv2.GaussianBlur(gray, (5,5), 0)
 edged = cv2.Canny(blur, 10, 100)
-
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
 
 dilate = cv2.dilate(edged, kernel, iterations=1)
 
-contours, _ = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-image_copy = image.copy()
+contours = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-cv2.drawContours(image_copy, contours, -1, (0, 255, 0), 2)
+contours =  contours[0] if len(contours) == 2 else contours[1]
+image_number = 0
 
+newFile = open("cardTitles.txt".format(image_number), "w+")
+for c in contours:
+    x,y,w,h = cv2.boundingRect(c)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (36, 255, 12), 3)
+    ROI = original_image[y:y+h, x:x+w]
+    if(ROI.shape[0] > 250 and ROI.shape[0] < 270 and ROI.shape[1] > 175 and ROI.shape[1] < 195 and ROI.shape[2] > 0 and ROI.shape[2] < 5):
+        cv2.imwrite("ROI_{}.png".format(image_number), ROI)
+        custom_config = r'--oem 3 --psm 6'
+        newFile.write(pytesseract.image_to_string("ROI_{}.png".format(image_number), config = custom_config).splitlines()[0])
+        newFile.write("\n")
+    image_number += 1
+
+newFile.close()
 cv2.imshow("image", image)
-cv2.imshow("edged", edged)
-cv2.imshow("Contours", image_copy)
 
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
+
+
